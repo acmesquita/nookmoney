a2a124df2c7c33f156f0070537e3fae54a3e12c0
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-10-21 08:50:27 -0300

    test: ensure load user be correctly

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,7 @@ module.exports = {
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/app/$1',
     '~tests/(.*)': '<rootDir>/tests/$1'
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/app/**/*.{ts,tsx}'],
 }
