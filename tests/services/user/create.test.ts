549f11a81cc4fb58ba81581dc08f61b271dcdd50
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-10-21 08:23:56 -0300

    test: ensure service create a new User when call with valid params

--- tests/services/user/create.test.ts
+++ tests/services/user/create.test.ts
@@ -3,6 +3,7 @@ import prisma from "~tests/config/client"
 import { InvalidParams } from "~/errors/invalid-params.error"
 import { CreateUser } from "~/services/user/create"
 import { getError, NoErrorThrownError } from "~tests/config/utils"
+import { prismaMock } from "~tests/config/singleton"
 
 describe('Create User', () => {
   it('when call without params, returns throw InvalidParams Error', async () => {
@@ -19,6 +20,29 @@ describe('Create User', () => {
 
     expect(error).not.toBeInstanceOf(NoErrorThrownError)
     expect(error).toBeInstanceOf(InvalidParams)
+  })
+
+  it('when call with correctly params, returns a new User', async () => {
+    const createUserService = new CreateUser(prisma)
+    const params = {
+      name: 'Fulano',
+      username: 'fulano',
+      password: '123456'
+    }
+
+    prismaMock.user.create.mockResolvedValue({
+      id: '1',
+      goalId: '',
+      passwordHash: '123456',
+      name: 'Fulano',
+      username: 'fulano',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+
+    const result = await createUserService.execute(params)
+
+    expect(result?.id).toBe('1')
   })
 })
 
