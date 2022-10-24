a913eb9566f1b47c78e8bb1cd7a9621142593715
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-10-24 12:09:48 -0300

    chore: add testing library

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/config/singleton.ts'],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/app/$1',
-    '~tests/(.*)': '<rootDir>/tests/$1'
+    '~tests/(.*)': '<rootDir>/tests/$1',
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/app/**/*.{ts,tsx}'],
