f12e622231f1a48b44be985d31f5d5d7801351d8
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-10-24 12:28:40 -0300

    chore: setupTest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/config/singleton.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/tests/config/setupTests.ts'
+  ],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/app/$1',
     '~tests/(.*)': '<rootDir>/tests/$1',
@@ -12,7 +14,15 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/app/**/*.{ts,tsx}'],
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest', {
+      jsc: {
+        transform: {
+          react: {
+            runtime: 'automatic',
+          },
+        },
+      },
+    },],
   },
   coveragePathIgnorePatterns: [
     '<rootDir>/app/config'
