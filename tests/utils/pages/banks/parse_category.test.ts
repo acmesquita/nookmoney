27ca84557179c97ed75ee6c43839e5879002de3c
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-10-24 11:45:30 -0300

    test: ensure ParseCategory to be correctly

---
+++ tests/utils/pages/banks/parse_category.test.ts
@@ -0,0 +1,12 @@
+import { parseCategory } from "~/utils/pages/banks/parse_category"
+
+describe('Parse Category', () => {
+  it('when call with invalid category', () => {
+    //@ts-ignore
+    expect(parseCategory('invalid')).toBe('')
+  })
+
+  it('when call with valid category when kind is conta_corrente', () => {
+    expect(parseCategory('conta_corrente')).toBe('Conta Corrente')
+  })
+})
